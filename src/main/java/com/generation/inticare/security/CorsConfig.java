7248a47c5fa2a9d09b20e75cb9c58795905df954
Guilherme <guilhermesoaresilvat@gmail.com>
2024-08-23 11:15:19 -0300

    atualização do CORS

--- src/main/java/com/generation/inticare/security/CorsConfig.java
+++ src/main/java/com/generation/inticare/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")  // Especifique a origem permitida
+                .allowedOrigins("https://inticare.netlify.app/")  // Especifique a origem permitida
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);  // Permite credenciais
